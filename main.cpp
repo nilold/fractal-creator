4b0c83e34b1a17927555ff4e2471148e514fd097
Nilo Neto <nilo.neto@avenuecode.com>
2020-05-05 19:57:36 -0300

    better mandelbrot

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <math.h>
 
 #include "Bitmap.h"
 #include "Mandelbrot.h"
@@ -21,20 +22,39 @@ int main() {
 
             int iterations = Mandelbrot::getIterations(xFractal, yFractal);
 
+            fractal[y * WIDTH + x] = iterations;
+
             if (iterations != Mandelbrot::MAX_ITERATIONS)
                 histogram[iterations]++;
-
-            uint8_t color = (uint8_t) (256 * (double) iterations / Mandelbrot::MAX_ITERATIONS);
-
-            color = color * color * color;
-
-            bitmap.setPixel(x, y, 0, color, 0);
-
         }
     }
 
+    int total = 0;
     for (int i = 0; i < Mandelbrot::MAX_ITERATIONS; ++i) {
-        std::cout << histogram[i] << " " << std::flush;
+        total += histogram[i];
+    }
+
+    for (int y = 0; y < HEIGHT; ++y) {
+        for (int x = 0; x < WIDTH; ++x) {
+            uint8_t red = 0;
+            uint8_t green = 0;
+            uint8_t blue = 0;
+
+
+            int iterations = fractal[y * WIDTH + x];
+
+            if (iterations != Mandelbrot::MAX_ITERATIONS) {
+                double hue = 0;
+
+                for (int i = 0; i <= iterations; i++) {
+                    hue += ((double) histogram[i]) / total;
+                }
+
+                green = pow(255, hue);
+            }
+
+            bitmap.setPixel(x, y, red, green, blue);
+        }
     }
 
 
